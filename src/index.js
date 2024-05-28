939da97e912b230ffaed7c916b6d2df0e143669b
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-05-28 22:03:23 +0200

    vcb cvn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { initMongoConnection } from './db/initMongoConnection.js';
+import { initMongoConnection } from './db/models/initMongoConnection.js';
 import { setupServer } from './server.js';
 
 const bootstrap = async () => {
