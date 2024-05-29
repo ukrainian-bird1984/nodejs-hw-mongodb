09d54b4b1dba7a5696880edf1f2e612cfd65946f
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-05-29 21:08:22 +0200

    fhnvm

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const ENV_VAR = {
   PORT: '3000',
-  MONGODB_USER: '<username>',
-  MONGODB_PASSWORD: '<password>',
+  MONGODB_USER: 'nadiiabasaraba84',
+  MONGODB_PASSWORD: '1984n1946cf',
   MONGODB_URL: 'cluster0.pty0hdj.mongodb.net',
   MONGODB_DB: 'contacts',
 };
