6a139bbdd9abbddec17820a783f04735b49c3aa0
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-05-29 18:30:47 +0200

    kbgjgb

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,5 +3,5 @@ export const ENV_VAR = {
   MONGODB_USER: '<username>',
   MONGODB_PASSWORD: '<password>',
   MONGODB_URL: 'cluster0.pty0hdj.mongodb.net',
-  MONGODB_DB: 'https://cloud.mongodb.com/v2/664fac1f23d48806bdfd4e3f#/clusters',
+  MONGODB_DB: 'contacts',
 };
