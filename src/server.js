feb9b931767c211fde495eb59f2e5863a84af3a9
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-06-06 22:54:12 +0200

    nbjhb

--- src/server.js
+++ src/server.js
@@ -1,18 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-<<<<<<< HEAD
-import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
-import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
-import mongoose from 'mongoose';
-
-//
-const PORT = Number(env('PORT', '3000'));
-export const setupServer = () => {
-  const app = express();
-  app.use(cors());
-=======
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { env } from './utils/env.js';
 import { contactsRouter } from './routers/contacts.js';
@@ -24,7 +12,6 @@ export const setupServer = () => {
 
   app.use(cors());
 
->>>>>>> 3ddb17fece38637224ec0c4fc70a44cab55f5385
   app.use(
     pino({
       transport: {
@@ -32,55 +19,6 @@ export const setupServer = () => {
       },
     }),
   );
-<<<<<<< HEAD
-  app.get('/contacts', async (req, res) => {
-    try {
-      const contacts = await getAllContacts();
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      res.status(500).json({
-        status: 500,
-        message: 'Failed to retrieve contacts',
-        error: error.message,
-      });
-    }
-  });
-  app.get('/contacts/:contactsId', async (req, res) => {
-    const contactId = req.params.contactsId;
-    try {
-      if (!mongoose.isValidObjectId(contactId)) {
-        return res.status(404).json({
-          status: 404,
-          message: `Id ${contactId} is not valid`,
-        });
-      }
-      const contact = await getContactById(contactId);
-      if (!contact) {
-        return res.status(404).json({
-          status: 404,
-          message: `Contact with id ${contactId} not found!`,
-        });
-      }
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (error) {
-      res.status(404).json({
-        status: 500,
-        message: `Something went wrong!`,
-        error: error.message,
-      });
-    }
-  });
-
-  app.use(notFoundMiddleware);
-=======
 
   app.use(
     express.json({
@@ -94,7 +32,6 @@ export const setupServer = () => {
   app.use(notFoundHandler);
 
   app.use(errorHandler);
->>>>>>> 3ddb17fece38637224ec0c4fc70a44cab55f5385
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,13 +1,5 @@
 import { ContactsCollection } from '../db/models/contact.js';
 
-<<<<<<< HEAD
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
-};
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
-=======
 export const getAllContacts = async () => await ContactsCollection.find();
 
 export const getContactById = async (contactId) =>
@@ -29,6 +21,5 @@ export const deleteContact = async (contactId) => {
   const contact = await ContactsCollection.findOneAndDelete({
     _id: contactId,
   });
->>>>>>> 3ddb17fece38637224ec0c4fc70a44cab55f5385
   return contact;
 };
