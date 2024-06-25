af32b9b46591638f26398b2f8b65d397453b4284
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-06-25 18:38:47 +0200

    last4

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -23,7 +23,6 @@ export const CLOUDINARY = {
 export const APP_DOMAIN = 'APP_DOMAIN';
 export const JWT_SECRET = 'JWT_SECRET';
 export const BACK_DOMAIN = 'BACK_DOMAIN';
-
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
