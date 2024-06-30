cc7c29f3a024b88c323ab8fe501726ea4cf6a5c7
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-06-30 13:19:29 +0200

    пртпро

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,5 +1,8 @@
 import createHttpError from 'http-errors';
-import { SessionCollection } from '../db/models/session.js';
+///
+import { SessionCollection } from '../../db/models/session.js';
+///
+
 import { UsersCollection } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
